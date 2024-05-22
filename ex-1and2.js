1238431ca4715fd7a17b7cca801379a640901790
Warat <warat.sat@gmail.com>
2024-05-22 19:30:52 +0700

    Update assignment

--- ex-1and2.js
+++ ex-1and2.js
@@ -1,5 +1,12 @@
 //Exercise #1: Variable Creation
-//Start coding here
+let firstName = "Karter";
+let lastName = "Anderson";
+let yourAddress = "54/24 Gp 5 Bangkruay-Sainoi Bang Kruay Bang Kruai";
+const restaurantName = "Chef's Table by Chef James";
+const foodPrice = 15000;
+const numOfRestaurantOpen = 1500;
 
 //Exercise #2: Variable Reassignment
-//Start coding here
+yourAddress = "(970) 878-53292400 County Rd #4Meeker, Colorado(CO), 81641";
+foodPrice = 5000;
+numOfRestaurantOpen = 2500;
